8ce254394b648622f1de6025095897d214a9fd19
qiuyueyang <qiuyueyang@baidu.com>
2018-11-26 14:18:14 +0800

    match pattern

--- src/com/qiuyang/scala/Test.scala
+++ src/com/qiuyang/scala/Test.scala
@@ -5,7 +5,16 @@ package com.qiuyang.scala
   */
 object Test {
   def main(args: Array[String]) {
-    println("hello scala")
+//    println("hello scala")
+    val list: List[Any] = List(12,'c',"hello",99.9)
+    list.foreach(matchFun)
   }
-
+  def matchFun(x:Any): Unit ={
+    x match{
+      case i:Int => println("This is Int "+i)
+      case s:String => println("This is String "+s)
+      case d:Double => println("This is Double "+d)
+      case _ => println("default ....")
+    }
+  }
 }
